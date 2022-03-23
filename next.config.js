d7c3027bcb45952ad2a41a6e8f13ecda69f8f09a
Ram kumar <webbieram@outlook.com>
2022-03-24 02:37:33 +0530

    ignore errors

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  ignoreBuildErrors: true,
 };
 
 module.exports = nextConfig;
