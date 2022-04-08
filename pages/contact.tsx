c85318758515f33da2103368e0abfaa8005c099e
Ram kumar <webbieram@outlook.com>
2022-04-08 13:40:21 +0530

    deploying

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -5,21 +5,20 @@ import { NextSeo } from 'next-seo';
 
 const ContactPage: NextPage = () => {
   return (
-    <>
+    <Container>
       <NextSeo title="Contact me" />
-      <Container>
-        <div className="md:px-32 md:pt-16 md:pb-32 text-center min-h-[60vh]">
-          <SendMailIcon />
-          <p className="mb-4 text-gray-600 dark:text-gray-400">
-            Let&apos;s work together <SmileIcon /> . To hire, collaborate or
-            give any feedback get in touch.
-          </p>
-          <h1 className="text-xl md:text-3xl underline">
-            devramthenmala@gmail.com
-          </h1>
-        </div>
-      </Container>
-    </>
+
+      <div className="md:px-32 md:pt-16 md:pb-32 text-center min-h-[60vh]">
+        <SendMailIcon />
+        <p className="mb-4 text-gray-600 dark:text-gray-400">
+          Let&apos;s work together <SmileIcon /> . To hire, collaborate or give
+          any feedback get in touch.
+        </p>
+        <h1 className="text-xl md:text-3xl underline">
+          devramthenmala@gmail.com
+        </h1>
+      </div>
+    </Container>
   );
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,31 +10,30 @@ import { NextSeo } from 'next-seo';
 
 const Home: NextPage = ({ post, featuredPost }) => {
   return (
-    <>
+    <Container>
       <NextSeo title="Home" />
-      <Container>
-        <div className="flex flex-col-reverse sm:flex-row items-start">
-          <div className="flex flex-col pr-8">
-            <div className="flex flex-col space-y-4">
-              <h1 className="text-6xl font-bold">Ram.</h1>
-            </div>
-            <h2 className="text-gray-700 dark:text-gray-200 mb-4">
-              Front end Full Stack Developer
-            </h2>
-            <p className="text-gray-600 dark:text-gray-400 mb-16">
-              I work with the JavaScript ecosystem. Welcome to my small piece of
-              the internet, where I write and share about different topics
-              related to the tech industry and life style.
-            </p>
+
+      <div className="flex flex-col-reverse sm:flex-row items-start">
+        <div className="flex flex-col pr-8">
+          <div className="flex flex-col space-y-4">
+            <h1 className="text-6xl font-bold">Ram.</h1>
           </div>
-          <div className="w-[80px] sm:w-[176px] relative mb-8 sm:mb-0 mr-auto"></div>
+          <h2 className="text-gray-700 dark:text-gray-200 mb-4">
+            Front end Full Stack Developer
+          </h2>
+          <p className="text-gray-600 dark:text-gray-400 mb-16">
+            I work with the JavaScript ecosystem. Welcome to my small piece of
+            the internet, where I write and share about different topics related
+            to the tech industry and life style.
+          </p>
         </div>
+        <div className="w-[80px] sm:w-[176px] relative mb-8 sm:mb-0 mr-auto"></div>
+      </div>
 
-        <GitPosts post={post} />
+      <GitPosts post={post} />
 
-        <FeaturedBlogs featuredPost={featuredPost} />
-      </Container>
-    </>
+      <FeaturedBlogs featuredPost={featuredPost} />
+    </Container>
   );
 };
 
